0d7bae2ac444e217a0e0029dc7b7c3d992808cd9
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-04-30 04:37:13 +0200

    Coming to life

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, {Component} from 'react';
-import Todos from './components/Todos'
+import Todos from './components/Todos';
+// import TodoItem from './components/TodoItem';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    todos:[
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+
+      },
+      {
+        id: 2,
+        title: "Let's code",
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Happy Hacking',
+        completed: true
+      },
+      {
+        id: 4,
+        title: "What's the end game?",
+        completed: false
+      }
+    ]
+  }
   render() {
+    // console.log(this.state.todos)
     return(
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
