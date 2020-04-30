71bbaad825d6e6249d73179e8bd1c864e72e3b05
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-04-30 03:14:25 +0200

    Let's do Reactjs

---
+++ src/components/Todos.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Todos extends Component {
+    render() {
+
+        return (
+            <div>
+                <h1>ToDos</h1>
+            </div>
+        );
+    }
+}
+
+export default Todos;
