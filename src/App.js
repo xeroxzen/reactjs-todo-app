98244b34c495b2478b35c9c328451a15da43bbf4
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-05-01 00:01:10 +0200

    Worked on AddTodo and added npm package 'uuid' to generate random IDs

--- src/App.js
+++ src/App.js
@@ -4,59 +4,76 @@ import HeaderItem from './components/layout/HeaderItem'
 import AddTodo from './components/AddTodo'
 // import TodoItem from './components/TodoItem';
 import './App.css';
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
-
   state = {
-    todos:[
+    todos: [
       {
-        id: 1,
-        title: 'Take out the trash',
-        completed: false
-
+        id: uuidv4(),
+        title: "Take out the trash",
+        completed: false,
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: "Let's code",
-        completed: false
+        completed: false,
       },
       {
-        id: 3,
-        title: 'Happy Hacking',
-        completed: false
+        id: uuidv4(),
+        title: "Happy Hacking",
+        completed: false,
       },
       {
-        id: 4,
+        id: uuidv4(),
         title: "What's the end game?",
-        completed: false
-      }
-    ]
-  }
+        completed: false,
+      },
+    ],
+  };
 
-  // Toggle Complete 
-  markComplete = (id) =>{
-    this.setState({ todos: this.state.todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo;
-    }) });
-  }
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
 
+  // Delete Todo
   delTodo = (id) => {
-    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]});
-  }
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+
+  // Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
 
   render() {
-    return(
+    return (
       <div className="App">
         <div className="container">
-
           <HeaderItem />
-          <AddTodo />
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
-
-        </div> 
+          <AddTodo addTodo={this.addTodo} />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
